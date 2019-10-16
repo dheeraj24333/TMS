2a44d4d92964afb0c34db4d71667ff75ba2363bc
Kiran Bhore <Kiran.Bhore@communitybrands.com>
2019-10-16 18:47:27 +0530

    are kiya na bhaiiiii

--- TMS/TMS.Designer.cs
+++ TMS/TMS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Maroof.Sheikh\source\repos\TMS\TMS\TMS.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Kiran.Bhore\source\repos\TMS3\TMS\TMS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
