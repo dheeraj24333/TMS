678076c5984e1c0d38dd67b680991488673f33a0
Maroof Sheikh <Maroof.Sheikh@communitybrands.com>
2019-10-18 12:06:53 +0530

    Login Chnages

--- TMS/Controllers/LoginController.cs
+++ TMS/Controllers/LoginController.cs
@@ -13,33 +13,33 @@ namespace TMS.Controllers
     {
         // GET: Login
         LoginPage st;
-        Boolean flag,flag1;
-       
+        Boolean flag, flag1;
+
         //
         // GET: /Home/
         [HttpGet]
         public ActionResult Index()
         {
 
-           
+
             return View();
         }
         [HttpPost]
 
         public ActionResult Index(LoginPage st)
         {
-            int[] results=new int[2];
+            int[] results = new int[2];
             flag = st.Login();
-            
+
             if (flag == true)
             {
                 return RedirectToAction("changePassword");
             }
-            if ( flag == false)
+            if (flag == false)
             {
                 return RedirectToAction("Dashboard");
             }
-           if(results[1]==2)
+            if (results[1] == 2)
             {
                 return RedirectToAction("error");
             }
@@ -52,17 +52,17 @@ namespace TMS.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult changePassword( LoginPage st)
+        public ActionResult changePassword(LoginPage st)
         {
-             flag1 = st.ChangePassword();
+            flag1 = st.ChangePassword();
             if (flag1 == true)
-               return  RedirectToAction("Profiles");
+                return RedirectToAction("Profiles");
             else
-               return RedirectToAction("error");
-            
+                return RedirectToAction("error");
+
             return View();
         }
-        
+
         public ActionResult error()
         {
             return View();
@@ -71,6 +71,13 @@ namespace TMS.Controllers
         [HttpGet]
         public ActionResult Profiles()
         {
+
+            training_management_systemEntities entities = new training_management_systemEntities();
+
+            manager man = new manager();
+            ViewBag.data = man.manager_name;
+
+
             if (Session["user_id"] == null)
             {
                 return RedirectToAction("Index");
@@ -80,24 +87,57 @@ namespace TMS.Controllers
         [HttpPost]
         public ActionResult Profiles(LoginPage profile)
         {
+
+            training_management_systemEntities entities = new training_management_systemEntities();
+
+            manager man = new manager();
+            ViewBag.data = man.manager_name;
+
+
             Boolean flag = profile.UpdateProfile();
             if (flag == true)
                 return RedirectToAction("Index");
             else
                 return RedirectToAction("error");
-            
-            
-        }
+            /*      string FileName = Path.GetFileNameWithoutExtension(profile.ImageFile.FileName);
 
-        public ActionResult routetodash()
-        {
-            return RedirectToAction("Index", "Dashboard");
-        }
+                  //To Get File Extension  
+                  string FileExtension = Path.GetExtension(profile.ImageFile.FileName);
 
-        public ActionResult reg()
-        {
-            ViewBag.Message = "Your registration is sent for comformation...";
-            return View("Dashboard");
+                  //Add Current Date To Attached File Name  
+                  FileName = DateTime.Now.ToString("yyyyMMdd") + "-" + FileName.Trim() + FileExtension;
+
+                  //Get Upload path from Web.Config file AppSettings.  
+                  string UploadPath = ConfigurationManager.AppSettings["UserImagePath"].ToString();
+
+                  //Its Create complete path to store in server.  
+                  profile.ImagePath = UploadPath + FileName;
+
+                  //To copy and save file into server.  
+                  profile.ImageFile.SaveAs(profile.ImagePath);
+
+                  //profile.ImageFile=
+                  int Session_id = 0;
+                      if (Session["user_id"] != null)
+                      {
+                          Session_id = Convert.ToInt32(Session["user_id"].ToString());
+
+                      }
+                      training_management_systemEntities entities = new training_management_systemEntities();
+                      company_employee emp = entities.company_employee.FirstOrDefault(e => e.user_id == Session_id);
+
+                      emp.Photograph = profile.ImagePath;
+                      entities.SaveChanges();
+                  }
+
+              else
+              {
+                  return RedirectToAction("error");
+              }*/
+
+
+
+            return View();
         }
 
         public ActionResult Dashboard()
@@ -122,7 +162,7 @@ namespace TMS.Controllers
 
                 return RedirectToAction("Index");
             }
-            
+            return View();
         }
 
     }
